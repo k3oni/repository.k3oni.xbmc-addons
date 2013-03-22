d406b1ecb257b02fbd9f3c61fe2cd22cd6157b0c
Florian N <michael@hostechs.com>
2013-03-21 20:26:29 -0400

    addons

---
+++ script.xbmc.pandora-master/README.txt
@@ -0,0 +1,37 @@
+Pandora Radio Script for XBMC
+
+This script is released freely without restrictions, but also without
+warrenty as described in the file "COPYING.txt"
+
+Usage:
+
+	Place in xbmc addons directory
+	Set login information in script properties (prompts on run if not set)
+	Pick a station and enjoy the music :)
+
+Credits:
+
+ - TWC Supplemental by Nuka1195
+	Used extensively as a reference for building the GUI.
+	Borrowed a few images for initial testing release.
+
+ - pianobar
+	Copyright (c) 2008-2010 Lars-Dominik Braun <PromyLOPh@lavabit.com>
+	http://github.com/PromyLOPh/pianobar | http://6xq.net/html/00/17.html
+		Uses as early reference for parts of the pandora protocol.
+
+ - jurmb84
+	Modified existing script to work with new xbmc addon framework.
+
+ - smorloc
+	Modified script to include like/dislike/tired functionality; additional
+	addon settings;layout changes
+
+ - htpc_guy
+	Skins
+
+ - VTWoods
+	Replaced libpandora with the pandora implementation from pithos.
+
+ - spbogie
+	Initial addon development.
---
+++ script.xbmc.pandora-master/pandaplayer.py
@@ -0,0 +1,52 @@
+import xbmc
+from threading import Timer
+
+class PandaPlayer( xbmc.Player ):
+
+	def __init__( self, core=None, panda=None ):
+		xbmc.Player.__init__( self, xbmc.PLAYER_CORE_MPLAYER )
+		self.panda = panda
+		self.timer = None
+
+	def playSong( self, item ):
+		print "PANDORA: Item 0 %s" % item[0]
+		print "PANDORA: Item 1 %s" % item[1]
+		self.play( item[0], item[1] )
+
+	def play( self, url, item ):
+		xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play( url, item )
+
+	def onPlayBackStarted( self ):
+		print "PANDORA: onPlayBackStarted: %s" %self.getPlayingFile()
+		if self.panda.playing:
+			if not "pandora.com" in self.getPlayingFile():
+			    if not "p-cdn.com" in self.getPlayingFile():
+				self.panda.playing = False
+				self.panda.quit()
+			else:
+				#Show Visualization (disappears after each song...)
+				xbmc.executebuiltin( "ActivateWindow( 12006 )" )
+
+	def onPlayBackEnded( self ):
+		print "PANDORA: onPlayBackEnded"
+		self.stop()
+		print "PANDORA: playing = %s" %self.panda.playing
+		if self.timer and self.timer.isAlive():
+			self.timer.cancel()
+		if self.panda.skip:
+			self.panda.skip = False
+		if self.panda.playing:
+			self.timer = Timer( 0.5, self.panda.playNextSong )
+			self.timer.start()
+
+	def onPlayBackStopped( self ):
+		print "PANDORA: onPlayBackStopped"
+		self.stop()
+		print "PANDORA: playing = %s" %self.panda.playing
+		if self.timer and self.timer.isAlive():
+			self.timer.cancel()
+		if self.panda.playing:
+			if self.panda.skip:
+				self.panda.skip = False
+			self.timer = Timer( 0.5, self.panda.playNextSong )
+			self.timer.start()
